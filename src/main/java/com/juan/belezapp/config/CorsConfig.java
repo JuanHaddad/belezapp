b3f76759b8f97c55397d8bc263374a804aa0753c
Juan Haddad <haddadjuan0@gmail.com>
2025-06-10 19:29:15 -0300

    adicionando a url certa no corsconfig

--- src/main/java/com/juan/belezapp/config/CorsConfig.java
+++ src/main/java/com/juan/belezapp/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://belezapp-frontend.onrender.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
